6a987013f51692a45d8b6b819c599834fe3db7b8
sumina729 <114402652+sumina729@users.noreply.github.com>
2022-10-10 19:43:49 +0900

    Update 2-2.c

--- 6week/2-2.c
+++ 6week/2-2.c
@@ -4,13 +4,19 @@
 int main() {
     int T;
     int H, W, N;
-    int finish_num;
 
     scanf("%d", &T);
 
     for(int i=0; i<T; i++){
         scanf("%d %d %d", &H, &W, &N);
-        printf("%d0%d", N%H, N/H+1);
+        if(N%H==0){
+          printf("%d%02d", H, N/H);
+        }
+        else
+        {
+          printf("%d%02d", N%H, N/H+1);
+        }
+      printf("\n");
     }
     return 0;
 }
